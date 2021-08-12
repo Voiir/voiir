9d6a9439f1df9832c06930f5c9376046697d0590
himanshumishra508 <mishrahimanshu.hmi@gmail.com>
2021-08-12 20:07:51 +0530

    Added environment variables

--- node_servers/app.js
+++ node_servers/app.js
@@ -3,12 +3,23 @@ const logger = require("morgan");
 const admin = require("firebase-admin");
 const middlew = require("express-firebase-middleware");
 const app = express();
+require('dotenv').config();
 
-var serviceAccount = require("./voiir-a23cb-firebase-adminsdk-8x041-38fcd8f12d.json");
 const { json } = require("express");
 
 var firebaseAdmin = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.project_key_id,
+    private_key: process.env.private_key,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+  }),
 });
 
 var firedb = firebaseAdmin.firestore();
