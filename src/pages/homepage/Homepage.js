d498be76eaaa23fd2bc5fd01fba34ad986cb0957
Naman Anand <voidmemories@Namans-MacBook-Air.local>
2021-08-29 00:08:32 +0530

    heroku revive

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -2,6 +2,24 @@ import homepageStyles from './Homepage.module.css';
 import searchIcon from '../../assets/search.svg';
 
 function Homepage(){
+  
+function getResults(){
+  var search = document.getElementById("searchBarID").value;
+  console.log(search);
+  fetch("https://voiir.herokuapp.com/api/userSearch/",{
+        method:'post',
+        headers:{'Content-Type': 'application/json',},
+        body:JSON.stringify({'name':search})
+    }).then(function(body){
+      console.log('in here');
+      return body.text(); // <--- THIS PART WAS MISSING
+    }).then(function(data) {
+      console.log(data);
+      
+    });
+
+}
+
     return (
         <div className={homepageStyles.homepage}>
           <div className={homepageStyles.searchBar}>
@@ -9,8 +27,8 @@ function Homepage(){
           <p style={{fontSize: '32px'}}>Yellow <span style={{color: '#9B72AA'}}>pages</span> of the 21st century.</p>
         </div>
         <form action>
-          <input type="text" className={homepageStyles.searchHint} placeholder="whom are you looking for?" />
-          <button type="submit" className={homepageStyles.button}>
+          <input type="text" className={homepageStyles.searchHint} id='searchBarID' placeholder="whom are you looking for?" />
+          <button type="submit" className={homepageStyles.button}  onClick={getResults}>
             <img src={searchIcon}></img>
           </button>
         </form>
