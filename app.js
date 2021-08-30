2c4921ab5f1cb752bc3f344c92f2fab403bfd105
himanshumishra508 <mishrahimanshu.hmi@gmail.com>
2021-08-30 21:37:28 +0530

    private key parse error correction

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var firebaseAdmin = admin.initializeApp({
     type: process.env.type,
     project_id: process.env.project_id,
     private_key_id: process.env.project_key_id,
-    private_key: process.env.private_key,
+    private_key: process.env.private_key.replace(/\\n/g, '\n'),
     client_email: process.env.client_email,
     client_id: process.env.client_id,
     auth_uri: process.env.auth_uri,
