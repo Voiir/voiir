f08a81eec167a14866f5a515c7fa8aed5398ffb5
Naman Anand <voidmemories@Namans-MacBook-Air.local>
2021-12-03 02:19:43 +0530

    merge force

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import "./App.css";
-import Routes from "./presentation/routes/routes";
-import Navbar from "./presentation/components/navbar/navbar";
-import Footer from "./presentation/components/footer/footer";
-import Loader from "./presentation/components/loader/loader";
+import './App.css';
+import Routes from './presentation/routes/routes';
+import Navbar from './presentation/components/navbar/navbar';
+import Footer from './presentation/components/footer/footer';
+import Loader from './presentation/components/loader/loader';
 
 function App() {
   return (
